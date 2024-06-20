17073f2409cf934b98339058348072624e54e005
elmrys <elmrys@gmail.com>
2024-06-20 18:31:00 +0200

    end of visualization 3

--- tests-mael/app.py
+++ tests-mael/app.py
@@ -23,6 +23,11 @@ names.drop(columns=['code'], inplace=True)
 
 just_names['annais'] = just_names['annais'].astype(int)
 
+def get_most_common_names(year_min, year_max, num_names=10) -> list[str]:
+    subset = just_names[(just_names['annais'] >= year_min) & (just_names['annais'] <= year_max)]
+    subset = subset.groupby(['preusuel'], as_index=False).sum()
+    subset = subset.sort_values(by='nombre', ascending=False)
+    return subset['preusuel'].head(num_names).tolist()
 
 def load_plain_data(name, min_year=1900, max_year=2020):
     print('hello')
@@ -111,11 +116,11 @@ def plot_name_all_years(name, min_year=1900, max_year=2020):
 
     total_births = grouped_data['nombre'].sum()
 
-    shadow_bar = alt.Chart(pd.DataFrame({'Cumulative Births': ['Total'], 'nombre': [total_births]})).mark_bar(
+    shadow_bar = alt.Chart(alt.Data(values=features)).mark_bar(
         color='lightgrey'
     ).encode(
         x=alt.X('Cumulative Births:N', title=''),
-        y=alt.Y('nombre:Q', title='nombre de naissances cumulées pour les départements sélectionnés')
+        y=alt.Y('sum(properties.nombre):Q', title='nombre de naissances cumulées pour les départements sélectionnés')
     ).properties(
         width=100,
         height=800
@@ -151,12 +156,22 @@ def plot_name_all_years(name, min_year=1900, max_year=2020):
 panel_name_input = pn.widgets.TextInput(name='Name', placeholder='Enter a name', value = 'Marie')
 min_year_input = pn.widgets.IntInput(name='Min year', placeholder = 'enter min year', value=1900)
 max_year_input = pn.widgets.IntInput(name='Max year', placeholder = 'enter max year', value=2020)
+information_panel = pn.pane.Markdown('Suggested 10 most common names in time period:')
+name_panels_column = pn.Column()
+
+def on_name_button_click(stringname):
+    panel_name_input.value = stringname
+    update_plot(panel_name_input.value, min_year_input.value, max_year_input.value)
 
 @pn.depends(panel_name_input, min_year_input, max_year_input, watch=True)
 def update_plot(name, min_year, max_year):
-    print('trying with', name, min_year, max_year)
     chart = plot_name_all_years(name, min_year, max_year)
-    print(chart)
+    most_common_names = get_most_common_names(min_year, max_year, 10)
+    name_panels_column.clear()
+    for common_name in most_common_names:
+        button = pn.widgets.Button(name=common_name)
+        button.on_click(lambda event, common_name=common_name: on_name_button_click(common_name))
+        name_panels_column.append(button)
     return chart
 
 chart_pane = pn.pane.Vega(update_plot, width=2500, height=1200)
@@ -166,7 +181,9 @@ app = pn.Row(
     pn.Column(
         panel_name_input,
         min_year_input,
-        max_year_input
+        max_year_input,
+        information_panel,
+        name_panels_column
     )
 )
 
